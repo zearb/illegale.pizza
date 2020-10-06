1ed0286a7cf7ad36efa528f7a0c3a0b70d7f2fe0
zearb <al2_ze@outlook.es>
2020-10-06 01:04:52 -0500

    x mientras rename docs

---
+++ docs/precache-manifest.fa59a651f6ae9b538fdcda5e5c9a50f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661b6f11a5c37fca658cd65a001e8bab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5c4ce6d8907eaff532",
+    "url": "/static/css/main.980bca3e.chunk.css"
+  },
+  {
+    "revision": "acd330c8e6e0ab56c372",
+    "url": "/static/js/2.68f0527d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.68f0527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5c4ce6d8907eaff532",
+    "url": "/static/js/main.638d39bf.chunk.js"
+  },
+  {
+    "revision": "bc68eaf2d463f76a5442",
+    "url": "/static/js/runtime-main.4c3c7f9e.js"
+  }
+]);
