2806534dadc08ce8803ffcba74bcba1ce0f3bc9c
Aldo <airaez@indracompany.com>
2021-05-26 02:17:24 -0500

    pizzaPuntos Build !

---
+++ docs/precache-manifest.fbd7b79a93bc1bd37611099ade028075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce024b18c2a838e8cb3255fc15c487b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0745144a3009a283e572",
+    "url": "/static/css/main.781ed662.chunk.css"
+  },
+  {
+    "revision": "cc580ebaf8fb2f681e70",
+    "url": "/static/js/2.88a0cab8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88a0cab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0745144a3009a283e572",
+    "url": "/static/js/main.19d4c976.chunk.js"
+  },
+  {
+    "revision": "bc68eaf2d463f76a5442",
+    "url": "/static/js/runtime-main.4c3c7f9e.js"
+  }
+]);
